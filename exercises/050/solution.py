9ca984a1360e4ed0b8b8b9ebbe623c868bd510f3
Mathilde <mathildesdx@gmail.com>
2015-09-21 17:44:01 +0200

    fait

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -7,11 +7,12 @@ Created on Mon Sep 21 14:43:32 2015
 
 liste = [0]
 
-for i in range(1, 999):
+for i in range(1, 1000):
     if (i % 3 == 0):
         liste.append(i)
-    if (i % 5 == 0):
-        liste.append(i)
+    else:
+        if (i % 5 == 0):
+            liste.append(i)
 
 result = sum(liste)
 print(result)
