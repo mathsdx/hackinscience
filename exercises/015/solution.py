88bafb5c03fcac75a3011d5da270db48c70fd4f2
Mathilde <mathildesdx@gmail.com>
2015-09-21 14:37:27 +0200

    fait

---
+++ exercises/015/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:30:15 2015
+
+@author: macbookmathilde
+"""
+
+result = 245850922 / 78256779
+result2 = (1 + 5 ** 0.5) / 2
+print(result)
+print(result2)
