be8ecf291794b6b8cfa97a1a7bacf84594de8721
Mathilde <mathildesdx@gmail.com>
2015-09-21 13:50:31 +0200

    fait

---
+++ exercises/013/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:30:15 2015
+
+@author: macbookmathilde
+"""
+
+liste = (2 + 2 == 5)
+
+if liste is True:
+    print("True")
+if liste is False:
+    print("False")
