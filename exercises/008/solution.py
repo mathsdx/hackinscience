f35f91531202a3115e9372a24bd9c571408ddfe8
Mathilde <mathildesdx@gmail.com>
2015-09-21 12:23:38 +0200

    fait

---
+++ exercises/008/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:30:15 2015
+
+@author: macbookmathilde
+"""
+
+list = ("H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", " ", "!")
+
+for i in list:
+    print(i)
