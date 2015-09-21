5aa157a8c1f1c2e3effe5feb586f5cb98e8bf105
Mathilde <mathildesdx@gmail.com>
2015-09-21 15:08:42 +0200

    fait

---
+++ exercises/019/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 14:43:32 2015
+
+@author: macbookmathilde
+"""
+
+import sys
+
+if len(sys.argv) <= 1:
+    print("usage: python3 solution.py OP1 OP2")
+else:
+    print(int(sys.argv[1]) + int(sys.argv[2]))
