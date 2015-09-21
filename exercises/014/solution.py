c6aad9053ea5e1ead8793f6dc07d748d90e223e2
Mathilde <mathildesdx@gmail.com>
2015-09-21 14:26:10 +0200

    fait

---
+++ exercises/014/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 11:30:15 2015
+
+@author: macbookmathilde
+"""
+
+import sys
+
+if len(sys.argv) <= 1:
+    print("usage: python3 solution.py PARAM")
+else:
+    print(sys.argv[1])
