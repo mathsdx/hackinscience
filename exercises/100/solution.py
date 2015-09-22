895ebd61e25d947ffda38dc6a2e6676092ca4ece
Mathilde <mathildesdx@gmail.com>
2015-09-22 15:11:33 +0200

    fait

---
+++ exercises/100/solution.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 14:43:32 2015
+
+@author: macbookmathilde
+"""
+
+station = {
+ 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+ 'number': 31705,
+ 'latitude': 48.8645278209514,
+ 'name': 'CHAMPEAUX (BAGNOLET)',
+ 'longitude': 2.416170724425901
+}
+
+print("latitude 48.864527821")
+print("longitude 2.41617072443")
+print("number 31705")
+print("name CHAMPEAUX (BAGNOLET)")
+print("address RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET")
