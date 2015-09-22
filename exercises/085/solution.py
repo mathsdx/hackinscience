940cf171a04153cb2c31d58c62b661b8248360f0
Mathilde <mathildesdx@gmail.com>
2015-09-22 14:07:20 +0200

    fait

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -10,21 +10,21 @@ import operator
 l = [1, 3, 2, 4, 6, 5, 9, 7]
 
 
-def sort_a_list():
+def sort_a_list(l):
     result = sorted(l, reverse=True)
-    print(result)
+    return(result)
 
 
-def sort_by_mark():
+def sort_by_mark(my_class):
     result = sorted(my_class, reverse=True)
-    print(result)
+    return(result)
 
-class1 = ([6, 'Joshua Tran'], [37, 'Jeanette Wafer'], [85, 'Susan Maddox'])
-class2 = ([84, 'Joseph Pedersen'], [12, 'Bonnie Torres'], [36, 'John Freeman'])
-class3 = ([27, 'Betty Askins'], [22, 'Mark Osbourne'], [42, 'Lidia Robel'])
+class1 = [[6, 'Joshua Tran'], [37, 'Jeanette Wafer'], [85, 'Susan Maddox']]
+class2 = [[84, 'Joseph Pedersen'], [12, 'Bonnie Torres'], [36, 'John Freeman']]
+class3 = [[27, 'Betty Askins'], [22, 'Mark Osbourne'], [42, 'Lidia Robel']]
 my_class = class1 + class2 + class3
 
 
-def sort_by_name():
+def sort_by_name(my_class):
     result = sorted(my_class, key=operator.itemgetter[2])
-    print(result)
+    return(result)
