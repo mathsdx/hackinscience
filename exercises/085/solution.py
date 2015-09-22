26563944aafb160f83bc7fb25a292b2ff11e130d
Mathilde <mathildesdx@gmail.com>
2015-09-22 13:58:30 +0200

    fait

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -26,5 +26,5 @@ my_class = class1 + class2 + class3
 
 
 def sort_by_name():
-    result = sorted(my_class, key=operator.itemgetter[2])    
+    result = sorted(my_class, key=operator.itemgetter[2])
     print(result)
