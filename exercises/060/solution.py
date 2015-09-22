9ccac5f3c436cf70c618284f638c55ae28a09f2c
Mathilde <mathildesdx@gmail.com>
2015-09-22 11:13:33 +0200

    fait

---
+++ exercises/060/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 14:43:32 2015
+
+@author: macbookmathilde
+"""
+
+for i in range(0, 26):
+    for j in range(0, 26):
+        print("abcdefghijklmnopqrstuvwxyz"[i], "abcdefghijklmnopqrstuvwxyz"[j])
